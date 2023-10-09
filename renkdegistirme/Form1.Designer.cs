2d1d8ecb2f15925fdb8e9b92d8b81930307a91bc
Luz <145450869+Q7nurs3vim@users.noreply.github.com>
2023-10-09 12:51:49 +0300

    Add files via upload

---
+++ renkdegistirme/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace renkdegistirme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbRenk = new System.Windows.Forms.CheckBox();
+            this.lblYaz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "gir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbRenk
+            // 
+            this.cbRenk.AutoSize = true;
+            this.cbRenk.Location = new System.Drawing.Point(35, 22);
+            this.cbRenk.Name = "cbRenk";
+            this.cbRenk.Size = new System.Drawing.Size(71, 17);
+            this.cbRenk.TabIndex = 1;
+            this.cbRenk.Text = "Mavi Yap";
+            this.cbRenk.UseVisualStyleBackColor = true;
+            // 
+            // lblYaz
+            // 
+            this.lblYaz.AutoSize = true;
+            this.lblYaz.Location = new System.Drawing.Point(32, 116);
+            this.lblYaz.Name = "lblYaz";
+            this.lblYaz.Size = new System.Drawing.Size(0, 13);
+            this.lblYaz.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(156, 152);
+            this.Controls.Add(this.lblYaz);
+            this.Controls.Add(this.cbRenk);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox cbRenk;
+        private System.Windows.Forms.Label lblYaz;
+    }
+}
+
